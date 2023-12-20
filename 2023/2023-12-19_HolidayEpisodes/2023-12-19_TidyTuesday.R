8e953c24a0bdb0e9737fbbd8d7950669473f0b94
Dailey <Hannah.Dailey@Waterboards.ca.gov>
2023-12-20 10:48:16 -0800

    update week 51

--- 2023/2023-12-19_HolidayEpisodes/2023-12-19_TidyTuesday.R
+++ 2023/2023-12-19_HolidayEpisodes/2023-12-19_TidyTuesday.R
@@ -5,10 +5,11 @@
 library(tidyverse)
 library(showtext)
 library(emojifont)
+library(cowplot)
 
 ## fonts
-# font_add_google("Mountains of Christmas", db_cache = FALSE)
-# showtext_auto()
+font_add_google("Nunito")
+showtext_auto()
 
 ## data
 tuesdata <- tidytuesdayR::tt_load(2023, week = 51)
@@ -33,52 +34,71 @@ episodes <- tuesdata$holiday_episodes %>%
                                     season_number == 12 ~ 0,
                                     season_number == 3 ~ 0.5,
                                     .default = 1)) %>%
-  mutate(cakePosition_y = case_when(season_number == 10 ~ 0.0085,
-                                    season_number == 4 ~ 0.0085,
-                                    season_number == 6 ~ -0.0032,
-                                    season_number == 9 ~ -0.0032,
-                                    season_number == 11 ~ -0.0032,
-                                    season_number == 5 ~ -0.0032,
-                                    average_rating == 7.4 ~ -0.0145,
-                                    season_number == 8 ~ -0.0145,
-                                    season_number == 12 ~ -0.0145,
-                                    season_number == 3 ~ -0.0145,
-                                    .default = -0.0145))
+  mutate(cakePosition_y = case_when(season_number == 10 ~ 0.022,
+                                    season_number == 4 ~ 0.022,
+                                    season_number == 6 ~ 0.007,
+                                    season_number == 9 ~ 0.007,
+                                    season_number == 11 ~ 0.007,
+                                    season_number == 5 ~ 0.007,
+                                    average_rating == 7.4 ~ -0.006,
+                                    season_number == 8 ~ -0.006,
+                                    season_number == 12 ~ -0.006,
+                                    season_number == 3 ~ -0.006,
+                                    .default = -0.006)) %>%
+  mutate(num_votes_category = case_when(num_votes <= 24 ~ "Less Than 24 Votes",
+                                        num_votes > 24 & num_votes <= 30 ~ "25-30 Votes",
+                                        num_votes > 30 ~ "More Than 30 Votes"),
+         num_votes_category = factor(num_votes_category))
   
-
-
-
-pal <- colorRampPalette(colors = c("#FF0000", "#FF7878", "#FFFFFF", "#74D680", "#178B29"))(25)
+episodeLegend <- data.frame(x = c(-1, 0, 1),
+                            y = rep(-0.04, 3),
+                            label = c("Less Than 24 Votes", "25-30 Votes", "More Than 30 Votes")) %>%
+  mutate(label = fct_inorder(label),
+         colourCode = case_when(label == "More Than 30 Votes" ~ '"#178B29"',
+                                label == "25-30 Votes" ~ '"#CCAD07"',
+                                label == "Less Than 24 Votes" ~ '"#FF0000"'))
 
 # Data Visualization ####
 plotStand <- ggplot() +
-  geom_segment(mapping = aes(x = -0.75, xend = 0.75, y = 0, yend = 0), linewidth = 8, colour = "grey65") +
-  geom_segment(mapping = aes(x = -1.25, xend = 1.25, y = -0.010, yend = -0.010), linewidth = 8, colour = "grey65") +
-  geom_segment(mapping = aes(x = -1.25, xend = 1.25, y = -0.020, yend = -0.020), linewidth = 8, colour = "grey65") +
-  geom_segment(mapping = aes(x = 0, xend = 0, y = 0.01, yend = -0.030), linewidth = 8, colour = "grey65") +
-  geom_point(mapping = aes(x = 0, y = 0.01), size = 12, colour = "grey65") +
-  geom_segment(mapping = aes(x = -0.3, xend = 0.3, y = -0.030, yend = -0.030), linewidth = 8, colour = "grey65")
+  geom_segment(mapping = aes(x = -0.75, xend = 0.75, y = 0, yend = 0), linewidth = 8, colour = "grey75") +
+  geom_segment(mapping = aes(x = -1.25, xend = 1.25, y = -0.010, yend = -0.010), linewidth = 8, colour = "grey75") +
+  geom_segment(mapping = aes(x = -1.25, xend = 1.25, y = -0.020, yend = -0.020), linewidth = 8, colour = "grey75") +
+  geom_segment(mapping = aes(x = 0, xend = 0, y = 0.01, yend = -0.030), linewidth = 8, colour = "grey75") +
+  geom_point(mapping = aes(x = 0, y = 0.01), size = 12, colour = "grey75") +
+  geom_segment(mapping = aes(x = -0.3, xend = 0.3, y = -0.030, yend = -0.030), linewidth = 8, colour = "grey75")
 
-plotFinal <- plotStand +
-  geom_text(data = episodes %>% filter(average_rating == 7.9), aes(x = cakePosition_x, y = cakePosition_y, colour = num_votes, label = emoji("cake")),
-            family = "EmojiOne", size = 46) +
-  geom_text(data = episodes %>% filter(average_rating == 7.8), aes(x = cakePosition_x, y = cakePosition_y, colour = num_votes, label = emoji("cake")),
-            family = "EmojiOne", size = 36) +
-  geom_text(data = episodes %>% filter(average_rating == 7.7), aes(x = cakePosition_x, y = cakePosition_y, colour = num_votes, label = emoji("cake")),
-            family = "EmojiOne", size = 36) +
-  geom_text(data = episodes %>% filter(average_rating <= 7.4), aes(x = cakePosition_x, y = cakePosition_y, colour = num_votes, label = emoji("cake")),
-            family = "EmojiOne", size = 28) +
-  ggrepel::geom_label_repel(data = episodes, aes(x = cakePosition_x, y = cakePosition_y, label = original_title), nudge_y = 0.0015)
+plotCake <- plotStand +
+  geom_text(data = episodes %>% filter(average_rating == 7.9), aes(x = cakePosition_x, y = cakePosition_y, colour = num_votes_category, label = emoji("cake")),
+            family = "EmojiOne", size = 84) +
+  geom_text(data = episodes %>% filter(average_rating == 7.8), aes(x = cakePosition_x, y = cakePosition_y, colour = num_votes_category, label = emoji("cake")),
+            family = "EmojiOne", size = 64) +
+  geom_text(data = episodes %>% filter(average_rating == 7.7), aes(x = cakePosition_x, y = cakePosition_y, colour = num_votes_category, label = emoji("cake")),
+            family = "EmojiOne", size = 64) +
+  geom_text(data = episodes %>% filter(average_rating <= 7.4), aes(x = cakePosition_x, y = cakePosition_y, colour = num_votes_category, label = emoji("cake")),
+            family = "EmojiOne", size = 52) +
+  scale_colour_manual(values = c("More Than 30 Votes" = "#178B29",
+                                 "25-30 Votes" = "#CCAD07",
+                                 "Less Than 24 Votes" = "#FF0000")) +
+  theme_void() +
+  theme(legend.position = "none",
+        plot.background = element_rect(fill = "#FFFFFF", colour = "#FFFFFF"))
   
 
-plotFinal <- episodes %>%
-  ggplot() +
-  geom_text(aes(x = factor(season_number), y = average_rating, colour = num_votes, label = emoji("cake")),
-               family = "EmojiOne", size = 45) +
-  geom_label(aes(x = factor(season_number), y = average_rating, label = str_to_title(primary_title)),
-                          min.segment.length = 10, nudge_y = 0.03, force = 5) +
-  scale_y_continuous(limits = c(7, 7.95)) +
-  scale_colour_gradient(low = "#FF0000", high = "#178B29", guide = "colourbar") +
+plotFinal <- plotCake +
+  geom_text(data = episodeLegend, mapping = aes(x = x, y = y, colour = label, label = emoji("cake")),
+            family = "EmojiOne", size = 52, lineheight = 0.05, fontface = "bold") +
+  geom_text(data = episodeLegend, mapping = aes(x = x, y = y - 0.002, colour = label, label = label),
+            size = 24, hjust = 0.5, family = "Nunito") +
+  scale_colour_manual(values = c("More Than 30 Votes" = "#178B29",
+                                 "25-30 Votes" = "#CCAD07",
+                                 "Less Than 24 Votes" = "#FF0000")) +
+  labs(title = "Great British Bakeoff Christmas Episodes",
+       subtitle = "Of the 11 Christmas themed Bakeoff episodes, two episodes have the highest average rating of 7.9 and five episodes having an average rating of less than 7.4.",
+       caption = "Source: IMDb | #TidyTuesday | Week 51 | @hdailey | Inspired by @nrennie") +
+  theme(text = element_text(family = "Nunito"),
+        plot.title = element_text(face = "bold", hjust = 0.5, size = 84),
+        plot.subtitle = ggtext::element_textbox_simple(lineheight = 0.3, halign = 0.5, size = 64, margin = margin(b = -100)),
+        plot.caption = element_text(size = 32, hjust = 0.5, colour = "grey35", margin = margin(t = 0, b = 2)))
   
 
 
@@ -86,3 +106,4 @@ plotFinal <- episodes %>%
 ggsave(plot = plotFinal, path = here::here("2023/2023-12-19_HolidayEpisodes/"),
        paste0(format(Sys.Date(), "%Y-%m-%d"), "_TT", ".png"), dpi = 300,
        height = 8, width = 11, unit = "in")
+
