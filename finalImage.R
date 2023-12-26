5c0957d2fc059e3ece2eb7bbd6aac66b0182658b
Dailey <Hannah.Dailey@Waterboards.ca.gov>
2023-12-26 09:10:07 -0800

    update year end 2023

--- finalImage.R
+++ finalImage.R
@@ -2,20 +2,22 @@ library(tidyverse)
 library(magick)
 
 # Change Year
-setwd(here::here("2022/"))
+setwd(here::here("2023/"))
 
-file.remove(here::here("2022/finalImage.png"))
+# Change Year
+file.remove(here::here("2023/finalImage.png"))
 
 img_list <- list.files(pattern = "\\.png$", recursive = TRUE, full.names = TRUE)
 img_list <- img_list[!img_list %in% c(img_list[41])]
 
 img_montage <- image_read(img_list) %>% 
-  image_montage(tile = "5x0",
+  image_montage(tile = "8x0",
                 geometry = "1000x1000+0+0",
                 shadow = FALSE, 
                 bg = "grey95")
 
+# Change Year
 image_write(img_montage, format = "png", 
             density = 300,
-            path = here::here("2022/finalImage.png"), 
+            path = here::here("2023/finalImage.png"), 
             quality = 100)
