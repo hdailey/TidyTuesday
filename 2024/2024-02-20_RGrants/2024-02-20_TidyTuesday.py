9c2edc1eb93f5537fa63f5044f8c6901cafca8dc
Dailey <Hannah.Dailey@Waterboards.ca.gov>
2024-02-21 16:28:09 -0800

    update week 8

---
+++ 2024/2024-02-20_RGrants/2024-02-20_TidyTuesday.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Tidy Tuesday - February 20, 2024
+"""
+
+import pandas as pd
+import os
+import numpy as np
+from plotnine import *
+from matplotlib import rc
+from mizani.formatters import dollar_format
+
+rc("font", **{"family":"serif", "serif":["Georgia"]})
+
+isc_grants = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-02-20/isc_grants.csv')
+
+isc_grantsByYear = isc_grants[["year", "funded"]].groupby(["year"]).sum().reset_index()
+isc_grantsMin = isc_grantsByYear.query("year == 2023")
+isc_grantsMax = isc_grantsByYear.query("year == 2018")
+
+# Plot Visualization
+plotFinal = (ggplot(isc_grantsByYear)
+             + aes(x = "year", y = "funded")
+             + geom_line(color = "white")
+             + geom_point(isc_grantsMin, aes(x = "year", y = "funded"), color = "yellow")
+             + geom_point(isc_grantsMax, aes(x = "year", y = "funded"), color = "yellow")
+             + geom_text(aes(x = 2018, y = 310000), label = "Maximum Funding (2018)\n$289,972", color = "yellow")
+             + geom_text(aes(x = 2021, y = 52000), label = "Minimum Funding (2023)\n$51,015", color = "yellow")
+             + geom_text(aes(x = 2021.5, y = 200000), label = "Funding is on\nthe decline", color = "yellow", size = 18)
+             + scale_y_continuous(labels = dollar_format(digits = 0, big_mark = ","))
+             + labs(x = "",
+                    y = "Total R Consortium ISC Grant\nFunding Awarded",
+                    title = "R Consortium ISC Grant Funding (2016-2023)",
+                    caption = "Source: R Consortium ISC | #TidyTuesday | Week 8 | @hdailey | Inspired by @curatedmess")
+             + theme_tufte()
+             + theme(text = element_text(family = "Georgia", color = "white", size = 18),
+                     plot_title = element_text(hjust = 0.5, size = 24, face = "bold"),
+                     plot_caption = element_text(hjust = 0.5, colour = "white", size = 6),
+                     axis_line = element_line(color = "white"),
+                     axis_title_y = element_text(size = 14, hjust = 0.5, lineheight = 1.5)))
+plotFinal
