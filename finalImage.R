c7f7a95708bf4aba070f37e15559d3b5b46f31a4
Hannah Dailey <91282117+hdailey@users.noreply.github.com>
2026-01-07 10:40:33 -0800

    Update finalImage.R

--- finalImage.R
+++ finalImage.R
@@ -2,16 +2,18 @@ library(tidyverse)
 library(magick)
 
 # Change Year
-setwd(here::here("2024/"))
+setwd(here::here("2025/"))
 
 # Change Year
-file.remove(here::here("2024/finalImage.png"))
+file.remove(here::here("2025/finalImage.png"))
 
 img_list <- list.files(pattern = "\\.png$", recursive = TRUE, full.names = TRUE)
-img_list <- img_list[!img_list %in% c(img_list[41])]
+
+# Remove non-plot images
+img_list <- img_list[!img_list %in% c(img_list[10], img_list[12], img_list[13])]
 
 img_montage <- image_read(img_list) %>% 
-  image_montage(tile = "8x0",
+  image_montage(tile = "4x0",
                 geometry = "1000x1000+0+0",
                 shadow = FALSE, 
                 bg = "grey99")
@@ -19,5 +21,5 @@ img_montage <- image_read(img_list) %>%
 # Change Year
 image_write(img_montage, format = "png", 
             density = 300,
-            path = here::here("2024/finalImage.png"), 
+            path = here::here("2025/finalImage.png"), 
             quality = 100)
